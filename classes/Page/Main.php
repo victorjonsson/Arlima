03bf279ec90513a8971ce830dedbc12c23d4b2be
aaslun <andreas.lundgren@norran.se>
2014-03-19 09:29:59 +0100

    ArlimaUtils.serverTime is now based on WordPress time, avoiding a time offset bug in search result.

--- classes/Page/Main.php
+++ classes/Page/Main.php
@@ -171,7 +171,7 @@ class Arlima_Page_Main extends Arlima_AbstractAdminPage {
         ?>
         <script>
             ArlimaArticle.defaultData = <?php echo json_encode(Arlima_ListFactory::createArticleDataArray()) ?>;
-            ArlimaUtils.serverTime = <?php echo time() * 1000; ?>;
+            ArlimaUtils.serverTime = <?php echo current_time('timestamp') * 1000; ?>;
             (function(win) {
                 var tmpls = [];
                 <?php foreach ($tmpl_resolver->getTemplateFiles()as $tmpl): ?>

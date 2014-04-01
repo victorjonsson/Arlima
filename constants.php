88bf11ca54b35291abb63a20e1ef8d63f9f695ec
Victor Jonsson <kontakt@victorjonsson.se>
2014-04-01 16:10:44 +0200

    built new version after merge with aaslun

--- constants.php
+++ constants.php
@@ -3,4 +3,4 @@
 define('ARLIMA_PLUGIN_PATH', dirname(__FILE__));
 define('ARLIMA_PLUGIN_URL', plugin_dir_url(__FILE__));
 defined('ARLIMA_DEV_MODE') or define('ARLIMA_DEV_MODE', false);
-define('ARLIMA_FILE_VERSION', '3.0.alpha.21' .(ARLIMA_DEV_MODE ? time():''));
+define('ARLIMA_FILE_VERSION', '3.0.alpha.22' .(ARLIMA_DEV_MODE ? time():''));

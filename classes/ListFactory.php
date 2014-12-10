f0e85df9d98a274d322be965fa846b18cca73c09
John Bergvall <john@sembly.se>
2014-12-10 10:25:30 +0100

    Use id instead of slug in test

--- classes/ListFactory.php
+++ classes/ListFactory.php
@@ -297,7 +297,7 @@ class Arlima_ListFactory {
         }
 
         // Reload list
-        $list = $this->loadList($list->getId(), false, true, true);
+        $list = $this->loadList($list->getId(), $version_id, true);
 
         if( !$preview ) {
             $this->cache->delete('arlima_list_articles_data_'.$list->getId());

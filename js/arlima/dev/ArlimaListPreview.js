1e902f99fcddce050a9b0d231951bdcb6f00e5ef
Victor Jonsson <kontakt@victorjonsson.se>
2015-02-24 16:40:38 +0100

    removed confusing query argument when viewing a list that is in a default state #53

--- js/arlima/dev/ArlimaListPreview.js
+++ js/arlima/dev/ArlimaListPreview.js
@@ -24,7 +24,7 @@ var ArlimaListPreview = (function($, window, ArlimaUtils, ArlimaArticleConnectio
 
             if( !list.hasUnsavedChanges() ) {
                 // add preview arg only so that Arlima adds jQuery to page
-                var listURL = _appendPreviewArgToURL(list.data.previewURL, 'yes');
+                var listURL = _appendPreviewArgToURL(list.data.previewURL, new Date().getTime(), '_');
                 _putWindowInfront(_createWindow(listURL));
                 return;
             }
@@ -102,8 +102,9 @@ var ArlimaListPreview = (function($, window, ArlimaUtils, ArlimaArticleConnectio
         }
     };
 
-    var _appendPreviewArgToURL = function(url, preview) {
-        return url +(url.indexOf('?') > -1 ? '&':'?')+ window.ArlimaJS.previewQueryArg+'='+preview;
+    var _appendPreviewArgToURL = function(url, preview, arg) {
+        arg = arg || window.ArlimaJS.previewQueryArg;
+        return url +(url.indexOf('?') > -1 ? '&':'?')+ arg +'='+preview;
     };
 
     var _addSaveMessage = function($windowBody) {
